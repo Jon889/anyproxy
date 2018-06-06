71bded05217e3dfab4848db23ce38a7787afe14b
Jonathan Bailey <jonathan.bailey13@imperial.ac.uk>
2018-06-06 17:47:03 +0530

    Add graphql sample file

---
+++ rule_sample/sample_graphqL_response.js
@@ -0,0 +1,39 @@
+const subs = require("./subs.json")
+
+const rewriters = [{
+  name: "CheckBasket",
+  rewriteResponse: function(bodyJSON) {
+    bodyJSON.data.basket.items[0].product.substitutions = subs
+  }
+},
+{
+  name: "GetFavourites",
+  rewriteResponse: function(bodyJSON) {
+    bodyJSON.data.favourites.productItems[0].substitutions = subs
+  }
+}]
+
+const GRAPHQL_ENDPOINT_URL = 'URL HERE';
+
+module.exports = {
+  summary: 'a rule to edit GraphQL query responses',
+  *beforeSendResponse(requestDetail, responseDetail) {
+    if (requestDetail.url === GRAPHQL_ENDPOINT_URL) {
+      const body = requestDetail.requestData.toString()
+      const newResponse = responseDetail.response;
+      const regex = /query ([^\(]*)\(/g;
+      const matches = regex.exec(body)
+      if (matches != null) {
+        const queryName = matches[1]
+        for (rewriter of rewriters) {
+          if (queryName == rewriter.name) {
+            let responseJSON = JSON.parse(newResponse.body)
+            rewriter.rewriteResponse(responseJSON)
+            newResponse.body = JSON.stringify(responseJSON)
+          }
+        }
+      }
+      return { response: newResponse }
+    }
+  },
+};
